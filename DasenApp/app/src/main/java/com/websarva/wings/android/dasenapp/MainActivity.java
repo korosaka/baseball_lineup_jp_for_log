a2edc378427060388eff706e2e7246ab64cc4906
koro1008 <koro1008.it@gmail.com>
2019-02-03 18:18:58 +0900

    レイアウト周りはだいたいOK

--- DasenApp/app/src/main/java/com/websarva/wings/android/dasenapp/MainActivity.java
+++ DasenApp/app/src/main/java/com/websarva/wings/android/dasenapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteStatement;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -32,14 +33,14 @@ public class MainActivity extends AppCompatActivity {
     public EditText etName;
     //登録ボタン
     Button record;
-//    キャンセルボタン
+    //    キャンセルボタン
     Button cancel;
     // 入れ替えボタン
     Button replace;
     // 入れ替え中フラグ
-    Boolean isReplacing;
+    Boolean isReplacing = false;
     // １つ目入れ替え選択フラグ
-    Boolean isFirstReplaceClicked;
+    Boolean isFirstReplaceClicked = false;
     //スタメンタイトル
     TextView title;
     //各打順の数字配列
@@ -75,6 +76,12 @@ public class MainActivity extends AppCompatActivity {
     //各打順の名前,ポジション用配列
     String[] names;
     String[] positions;
+
+    // 打順ボタン
+    Button[] number_buttons = new Button[9];
+
+    int firstClicked = -1;
+
 
     //ここからmain
     @Override
@@ -113,8 +120,19 @@ public class MainActivity extends AppCompatActivity {
         position7 = findViewById(R.id.position7);
         position8 = findViewById(R.id.position8);
         position9 = findViewById(R.id.position9);
+        number_buttons[0] = findViewById(R.id.btn1);
+        number_buttons[1] = findViewById(R.id.btn2);
+        number_buttons[2] = findViewById(R.id.btn3);
+        number_buttons[3] = findViewById(R.id.btn4);
+        number_buttons[4] = findViewById(R.id.btn5);
+        number_buttons[5] = findViewById(R.id.btn6);
+        number_buttons[6] = findViewById(R.id.btn7);
+        number_buttons[7] = findViewById(R.id.btn8);
+        number_buttons[8] = findViewById(R.id.btn9);
+
+
         //打順配列に打順番号入れる(1~19番)
-        for(int i = 0;i < 19;i++){
+        for (int i = 0; i < 19; i++) {
             numbers[i] = i + 1;
         }
         //スピナー紐付け
@@ -123,18 +141,18 @@ public class MainActivity extends AppCompatActivity {
         etName.setFocusable(false);
         etName.setFocusableInTouchMode(false);
         etName.setEnabled(false);
-        etName.setOnFocusChangeListener(new View.OnFocusChangeListener(){
+        etName.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
-            public void onFocusChange(View view,boolean flag){
+            public void onFocusChange(View view, boolean flag) {
 //                フォーカスを取得→キーボード表示
-                if(flag){
-                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-                    imm.showSoftInput(view,0);
+                if (flag) {
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.showSoftInput(view, 0);
                 }
 //                フォーカス外れる→キーボード非表示
                 else {
-                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-                    imm.hideSoftInputFromWindow(view.getWindowToken(),0);
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
                 }
             }
         });
@@ -148,7 +166,7 @@ public class MainActivity extends AppCompatActivity {
         names = new String[19];
         positions = new String[19];
         //9人分のデータ取り出し
-        for (int j = 0;j < 9;j++) {
+        for (int j = 0; j < 9; j++) {
             //ヘルパーから接続オブジェクト取得
             SQLiteDatabase db = helper.getReadableDatabase();
             try {
@@ -158,8 +176,7 @@ public class MainActivity extends AppCompatActivity {
                 //sql実行（カーソルオブジェクト（実行結果そのもの？）が戻り値）
                 Cursor cursor = db.rawQuery(sqlSelect, null);
                 //cursor.moveToNext() ➡︎データない時（移動ができなかった時）はfalseになる
-                if(cursor.moveToNext())
-                {
+                if (cursor.moveToNext()) {
                     //カラムのインデックス値取得
                     int idxName = cursor.getColumnIndex("playername");
                     int idxPosition = cursor.getColumnIndex("position");
@@ -167,7 +184,7 @@ public class MainActivity extends AppCompatActivity {
                     name = cursor.getString(idxName);
                     position = cursor.getString(idxPosition);
                     //名前の空白登録は未登録にする
-                    if(name.equals("")){
+                    if (name.equals("")) {
                         name = "-----";
                     }
                 }
@@ -208,56 +225,88 @@ public class MainActivity extends AppCompatActivity {
         position8.setText(positions[7]);
         position9.setText(positions[8]);
     }
+
     //以下１〜９番の打順ボタン処理⬇
-    public void onClick1(View view){
+    public void onClick1(View view) {
         commonMethod(0);
     }
-    public void onClick2(View view){
+
+    public void onClick2(View view) {
         commonMethod(1);
     }
-    public void onClick3(View view){
+
+    public void onClick3(View view) {
         commonMethod(2);
     }
-    public void onClick4(View view){
+
+    public void onClick4(View view) {
         commonMethod(3);
     }
-    public void onClick5(View view){
+
+    public void onClick5(View view) {
         commonMethod(4);
     }
-    public void onClick6(View view){
+
+    public void onClick6(View view) {
         commonMethod(5);
     }
-    public void onClick7(View view){
+
+    public void onClick7(View view) {
         commonMethod(6);
     }
+
     public void onClick8(View view) {
         commonMethod(7);
     }
-    public void onClick9(View view){
+
+    public void onClick9(View view) {
         commonMethod(8);
     }
+
     //打順ボタン共通メソッド（打順・登録状態表示、EditText・登録/クリアボタンの有効化、データベース用の数字登録）
-    public void commonMethod(int j){
+    public void commonMethod(int j) {
 
         // 入れ替え時のクリックと処理区別
-        if(isReplacing){
+        if (isReplacing) {
+
             // 入れ替え時
-            if(!isFirstReplaceClicked){
+            if (!isFirstReplaceClicked) {
                 // 1つめ選択時
-                // どのボタンがクリックされたか記憶
-                // そのボタンの色帰る
+
+                number_buttons[j].setTextColor(Color.parseColor("#FF0000"));
+                firstClicked = j;
+                isFirstReplaceClicked = true;
 
             } else {
                 // 2つめ選択時
 
-                // 同じボタンがクリックされた →　元に戻す
-                // 異なるボタン →入れ替え処理
+                if (j == firstClicked) {
+                    // 同じボタンがクリックされた →　元に戻す
 
-                // レイアウト場で入れ替え
-                // DB内で入れ替え
+                    number_buttons[j].setTextColor(Color.parseColor("#000000"));
+                    isFirstReplaceClicked = false;
+                    firstClicked = -1;
 
-                // 入れ替え中フラグもどす
-//        replace.setEnabled(false);
+                } else {
+                    // 異なるボタン →入れ替え処理
+
+                    // レイアウト場で入れ替え
+                    // DB内で入れ替え
+
+                    // 入れ替え中フラグもどす
+                    replace.setEnabled(false);
+                    // 色々戻す
+                    number_buttons[firstClicked].setTextColor(Color.parseColor("#000000"));
+
+                    isReplacing = false;
+                    isFirstReplaceClicked = false;
+                    firstClicked = -1;
+                    replace.setEnabled(true);
+                    cancel.setEnabled(false);
+                    title.setText(R.string.title);
+                    // TODO サブオーダーの時も
+
+                }
 
 
             }
@@ -269,9 +318,9 @@ public class MainActivity extends AppCompatActivity {
             String number = String.valueOf(numbers[j]) + "番";
             tvSelectNum.setText(number);
             //下記メソッド使用
-            setSpinner(spinner,positions[j + k]);
+            setSpinner(spinner, positions[j + k]);
             etName.setText(names[j + k]);
-            if(etName.getText().toString().equals("-----")){
+            if (etName.getText().toString().equals("-----")) {
                 etName.setText("");
             }
             etName.setEnabled(true);
@@ -287,12 +336,13 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
     //文字列からスピナーをセットするメソッド（上記メソッドで使用）
-    public void setSpinner(Spinner spinner,String position){
+    public void setSpinner(Spinner spinner, String position) {
         SpinnerAdapter adapter = spinner.getAdapter();
         int index = 0;
-        for(int i = 0; i < adapter.getCount(); i++){
-            if(adapter.getItem(i).equals(position)){
+        for (int i = 0; i < adapter.getCount(); i++) {
+            if (adapter.getItem(i).equals(position)) {
                 index = i;
                 break;
             }
@@ -302,10 +352,10 @@ public class MainActivity extends AppCompatActivity {
 
 
     //登録ボタン押した処理
-    public void onClickSave(View view){
+    public void onClickSave(View view) {
         //入力文字列取得
         String playerName = etName.getText().toString();
-        if(playerName.equals("")){
+        if (playerName.equals("")) {
             playerName = "-----";
         }
         //ポジション取得
@@ -321,7 +371,7 @@ public class MainActivity extends AppCompatActivity {
             //上記文字列からPreparedStatement取得（SQLを実行するためのインターフェース）
             SQLiteStatement stmt = db.compileStatement(sqlDelete);
             //変数バインド（数字は何番目の？に入れるか,？に入れるもの）（kはオプション選択によって変わる）
-            stmt.bindLong(1,numbers[i + k]);
+            stmt.bindLong(1, numbers[i + k]);
             //削除SQL実行
             stmt.executeUpdateDelete();
 
@@ -333,16 +383,14 @@ public class MainActivity extends AppCompatActivity {
             //変数バインド（数字は何番目の？に入れるか,？に入れるもの）
             //_id(primary key) は設定不要らしい
             //サブオーダー選択時は打順は11~19番でデータベースに登録される
-            stmt.bindLong(2,numbers[i + k]);
-            stmt.bindString(3,playerName);
-            stmt.bindString(4,position);
+            stmt.bindLong(2, numbers[i + k]);
+            stmt.bindString(3, playerName);
+            stmt.bindString(4, position);
             //インサートSQL実行
             stmt.executeInsert();
-        }
-        catch (Exception e){
-            Log.d("error","例外発生");
-        }
-        finally {
+        } catch (Exception e) {
+            Log.d("error", "例外発生");
+        } finally {
             //データ接続プブジェクト解放
             db.close();
         }
@@ -359,7 +407,7 @@ public class MainActivity extends AppCompatActivity {
         replace.setEnabled(true);
 
         //画面のメンバー表に反映（１〜９番まで）
-        switch (i){
+        switch (i) {
             case 0:
                 name1.setText(playerName);
                 position1.setText(position);
@@ -418,22 +466,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //クリアボタン処理
-    public void onClickClear(View view){
+    public void onClickClear(View view) {
         //入力名をクリア状態に
         etName.setText("");
         //スピナー（守備位置）を未選択状態に戻す
         spinner.setSelection(0);
     }
 
-//    キャンセルボタン処理
-    public void onClickCancel(View view){
+    //    キャンセルボタン処理
+    public void onClickCancel(View view) {
 
         // 入れ替えボタンクリック時のキャンセルor入力中のキャンセル？
-        if(isReplacing){
-            //打順ボタン色戻し
+        if (isReplacing) {
             //入れ替えボタン戻し
+            replace.setEnabled(true);
             //入れ替えフラグ戻し
-        }else {
+            isReplacing = false;
+            isFirstReplaceClicked = false;
+            firstClicked = -1;
+            // TODO サブオーダーの時も
+
+            title.setText(R.string.title);
+
+            if(isFirstReplaceClicked){
+                number_buttons[firstClicked].setTextColor(Color.parseColor("#000000"));
+            }
+
+        } else {
             //それぞれ初期状態に戻す
             tvSelectNum.setText(getString(R.string.current_num));
             etName.setText("");
@@ -444,24 +503,23 @@ public class MainActivity extends AppCompatActivity {
             record.setEnabled(false);
             cancel.setEnabled(false);
             clear.setEnabled(false);
+            replace.setEnabled(true);
         }
 
 
     }
 
     // 入れ替えボタン処理
-    public void onClickReplace(View view){
+    public void onClickReplace(View view) {
 
-        // 入れ替えボタンはenable(false)に(普通のfalseと区別するように色変えられないかな？？)
-        replace.setEnabled(false);
         // 入れ替えクリックされているフラグ(キャンセル用)
         isReplacing = true;
+        // 入れ替えボタンはenable(false)に(普通のfalseと区別するように色変えられないかな？？)
+        replace.setEnabled(false);
         // キャンセルはできるように
         cancel.setEnabled(true);
         // タイトルが『２つボタンクリック』になる
         title.setText(R.string.replace_title);
-        // いったんボタン全て色変わる
-
 
         // ここの処理はここまで、
         // 以下の処理はcommonMethod()内で色々処理しないと！！
@@ -471,27 +529,27 @@ public class MainActivity extends AppCompatActivity {
 
     //オプションメニュー追加
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         //メニューインフレター取得
         MenuInflater inflater = getMenuInflater();
         //オプションメニュー用.xmlファイルをインフレート（メニュー部品をJavaオブジェクトに）
-        inflater.inflate(R.menu.menu_options_menu_list,menu);
+        inflater.inflate(R.menu.menu_options_menu_list, menu);
         //親クラスの同名メソッドで返却
         return super.onCreateOptionsMenu(menu);
     }
 
     //オプションメニューを選択した時の処理
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
+    public boolean onOptionsItemSelected(MenuItem item) {
         //選択されたオプションメニューのID取得
         int itemId = item.getItemId();
         //IDのR値による処理分岐
         switch (itemId) {
-                //オーダー選択の場合
+            //オーダー選択の場合
             case R.id.oder:
                 k = 0;
                 break;
-                //サブオーダーの場合
+            //サブオーダーの場合
             case R.id.subOder:
                 //データベース上の打順が11〜19番を対象に
                 k = 10;
@@ -501,14 +559,14 @@ public class MainActivity extends AppCompatActivity {
                 String[] positionIntent = new String[9];
                 String[] nameIntent = new String[9];
                 //送るデータ（9人分）を抽出（正規orサブ）
-                for (int i = 0;i < 9;i++){
+                for (int i = 0; i < 9; i++) {
                     positionIntent[i] = positions[i + k];
                     nameIntent[i] = names[i + k];
                 }
                 //フィールド画面へ
-                Intent intent = new Intent(MainActivity.this,FieldActivity.class);
-                intent.putExtra("positions",positionIntent);
-                intent.putExtra("names",nameIntent);
+                Intent intent = new Intent(MainActivity.this, FieldActivity.class);
+                intent.putExtra("positions", positionIntent);
+                intent.putExtra("names", nameIntent);
                 startActivity(intent);
                 break;
         }
@@ -517,33 +575,29 @@ public class MainActivity extends AppCompatActivity {
         DatabaseHelper helper = new DatabaseHelper(MainActivity.this);
         String name;
         String position;
-        for (int j = 0;j < 9;j++) {
+        for (int j = 0; j < 9; j++) {
             SQLiteDatabase db = helper.getReadableDatabase();
             try {
                 //SQL文字列作成➡︎検索・表示（1~9番 or 11~19番）(mainメソッドとほぼ同じ)
                 String sqlSelect = "SELECT playername,position FROM batting WHERE number = " + numbers[j + k];
                 Cursor cursor = db.rawQuery(sqlSelect, null);
-                if(cursor.moveToNext())
-                {
+                if (cursor.moveToNext()) {
                     int idxName = cursor.getColumnIndex("playername");
                     int idxPosition = cursor.getColumnIndex("position");
                     name = cursor.getString(idxName);
                     position = cursor.getString(idxPosition);
-                    if(name.equals("")){
+                    if (name.equals("")) {
                         name = "-----";
                     }
-                }
-                else {
+                } else {
                     name = "-----";
                     position = "----";
                 }
                 names[j + k] = name;
                 positions[j + k] = position;
-            }
-            catch (Exception e) {
+            } catch (Exception e) {
                 Log.e("キャッチ2", "エラー2", e);
-            }
-            finally {
+            } finally {
                 db.close();
             }
         }
@@ -566,10 +620,9 @@ public class MainActivity extends AppCompatActivity {
         position8.setText(positions[7 + k]);
         position9.setText(positions[8 + k]);
         //見出し変更
-        if(k == 0){
+        if (k == 0) {
             title.setText("~Starting Member~");
-        }
-        else{
+        } else {
             title.setText("~Starting Member~（サブ）");
         }
         //上部入力欄初期状態へ
